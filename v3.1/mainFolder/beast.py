59d02643b1c445e50fb7e8a1b230c7d8215dd321
Lucky Sagar <sanjupdw.duhatand@gmail.com>
2026-08-24 16:37:11 +0530

    Create beast.py
    This file defines various beast classes and initializes instances for different game stages. It also handles loading and saving player experience data.

---
+++ v3.1/mainFolder/beast.py
@@ -0,0 +1,76 @@
+import json
+import random
+
+class Beast:
+    def __init__(self, name, health, attackDamage, difficulty):
+        self.name = name
+        self.health = health
+        self.attackDamage = attackDamage
+        self.difficulty = difficulty
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+class EarlyBeast(Beast):
+    pass
+
+class MidgameBeast(Beast):
+    pass
+
+class EndgameBeast(Beast):
+    pass
+
+class Boss(Beast):
+    pass
+
+gloomfang = EarlyBeast("Gloomfang", 15, 10, "Easy")
+thornback = EarlyBeast("Thornback", 25, 35, "Easy")
+ashcrawler = EarlyBeast("Ashcrawler", 35, 65, "Easy")
+
+vexmax = MidgameBeast("Vexmax", 95, 105, "Medium")
+dreadhorn = MidgameBeast("Dreadhorn", 105, 110, "Medium")
+mirewretch = MidgameBeast("Mirewretch", 120, 115, "Medium")
+
+stonecrusher = EndgameBeast("Stonecrusher", 125, 120, "Hard")
+voidclaw = EndgameBeast("Voidclaw", 100, 135, "Hard")
+emberstalker = EndgameBeast("Emberstalker", 150, 55, "Hard")
+
+ravakor = Boss("Ravakor", 350, 400, "Impossible")
+ 
+earlyBeasts = [gloomfang, thornback, ashcrawler]
+midgamebeasts = [vexmax, dreadhorn, mirewretch]
+endgamebeasts = [stonecrusher, voidclaw, emberstalker]
+theBoss = [ravakor]
+
+# Beasts
+midEnemy = midgamebeasts + earlyBeasts
+endEnemy = endgamebeasts + midgamebeasts + earlyBeasts
+finalBoss = theBoss + endgamebeasts + midgamebeasts + earlyBeasts
+
+try:
+    with open("mainFolder/save.json", "r") as f:
+        data = json.load(f)
+        level = data["level"]
+        experience = data["experience"]
+
+except (json.JSONDecodeError, FileNotFoundError, KeyError):
+    data = {"level": 1, "experience": 0}
+    level = data["level"]
+    experience = data["experience"]
+    with open("mainFolder/save.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+if level < 10:
+    enemy = random.choice(earlyBeasts)
+elif level < 30:
+    enemy = random.choice(midEnemy)
+elif level < 50:
+    enemy = random.choice(endEnemy)
+else:
+    enemy = random.choice(finalBoss)
+
+enemyHealth = enemy.health
+enemyAttackPower = enemy.attackDamage
